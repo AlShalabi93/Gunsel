0ea7d35477ffcbc14b9768ac886f4e5fff56f166
Mohamad Alshalabi <mohamad.alshalabi@gunsel.local>
2024-01-24 16:47:20 +0200

    My first project

---
+++ my-rich-text-editor/src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './App.css';
+import RichTextEditor from "./components/RichTextEditor" ;
+
+
+function App() {
+  return (
+    <div className="App">
+      Helli 
+     <RichTextEditor/>
+    </div>
+  );
+}
+
+export default App;
